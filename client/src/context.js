bd7d1cd57d12265618ee8fd027892c998c885d7d
Sean Yang <seanyixuan@gmail.com>
2021-11-25 19:23:11 -0800

    got axios get call working for getting top 10 songs in the US charts

--- client/src/context.js
+++ client/src/context.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 const Context = React.createContext();
+const apiKey = '400a847c3fba710765829132d1b9052b';
 
 export class Provider extends Component {
     state = {
@@ -10,6 +12,15 @@ export class Provider extends Component {
         ],
         heading: 'Trending Tracks'
     };
+
+    componentDidMount() {
+        axios.get(`https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${apiKey}`)
+            .then(res => {
+                console.log(res.data);
+                this.setState({trackList: res.data.message.body.track_list});
+            })
+            .catch(err => console.log(err));
+    }
 
     render() {
     return (
